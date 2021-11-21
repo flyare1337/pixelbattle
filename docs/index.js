9ee402b13d06a68d77fc3f8202800e64d1eb3d20
vlfz <senioreighteen@yandex.ru>
2021-11-21 22:02:09 +0500

    localhost fix

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,4 @@
-const hostname = "https://pixels-api.boticord.top";
+const hostname = "http://localhost:9000";
 const processedErrors = (type, args) => {
     let types = {
         "IncorrectColor": "Указан некорректный код цвета.",
