400cc988c8e7f40117e4437024c044537da7176b
bit0r1n <me@bitor.in>
2021-11-23 16:39:57 +0300

    end event

--- src/routes/ssePixels.js
+++ src/routes/ssePixels.js
@@ -1,3 +1,4 @@
+const { ended } = require('../config.json');
 const { on } = require('events');
 
 module.exports = (r, ee) => ({
@@ -13,6 +14,7 @@ module.exports = (r, ee) => ({
     handler(req, res) {
         res.sse(
             (async function* () {
+                if (ended) yield { data: JSON.stringify({ op: 'ENDED', value: true }) }
                 for await (const [data] of on(ee, 'place')) {
                     yield { data: JSON.stringify(data) };
                 }
